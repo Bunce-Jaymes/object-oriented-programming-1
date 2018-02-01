3993c08e3135b89bcfad4ed66428f92661296095
Jaymes <35280742+PoisonedAlien@users.noreply.github.com>
2018-02-01 12:54:46 -0700

    Update

---
+++ getOut/src/cit260/getOut/model/Game.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cit260.getOut.model;
+import java.io.Serializable;
+import java.util.Objects;
+/**
+ *
+ * @author jayme
+ */
+public class Game implements Serializable{
+    private double startTime;
+    private String description;
+    private int noPlayer;
+    private Player player;
+
+    public Game() {
+    }
+    
+     public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+   
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getNoPlayer() {
+        return noPlayer;
+    }
+
+    public void setNoPlayer(int noPlayer) {
+        this.noPlayer = noPlayer;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "description=" + description + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + (int) (Double.doubleToLongBits(this.startTime) ^ (Double.doubleToLongBits(this.startTime) >>> 32));
+        hash = 23 * hash + Objects.hashCode(this.description);
+        hash = 23 * hash + this.noPlayer;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (Double.doubleToLongBits(this.startTime) != Double.doubleToLongBits(other.startTime)) {
+            return false;
+        }
+        if (this.noPlayer != other.noPlayer) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
