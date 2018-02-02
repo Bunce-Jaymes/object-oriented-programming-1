5d96f0fd24d6525926dfe102a299566467e7f318
sam-prettyman <35538856+sam-prettyman@users.noreply.github.com>
2018-02-01 18:04:13 -0700

    actor class update
    actor class update

--- getOut/src/cit260/getOut/model/Actor.java
+++ getOut/src/cit260/getOut/model/Actor.java
@@ -10,12 +10,35 @@ package cit260.getOut.model;
  * @author Samuel Prettyman
  */
 public enum Actor{
+    
+    Vitcim("Raphael", "He has been captured by Sinaloa Cartel", new Point(0,0));
+    
     private String name;
     private String description;
     private Point coordinates;
 
-Actor(String name, String description, Point coordainates) {
-this.name = name;
-this.description = description;
-this coordinates = coordinates;
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Point getCoordinates() {
+        return coordinates;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Actor{" + "name=" + name + ", description=" + description + ", coordinates=" + coordinates + '}';
+    }
+    
+    Actor(String name, String description, Point coordainates) {
+        this.name = name;
+        this.description = description;
+        this.coordinates = coordinates;
 }
+}
+
--- getOut/src/cit260/getOut/model/Game.java
+++ getOut/src/cit260/getOut/model/Game.java
@@ -79,3 +79,4 @@ public class Game implements Serializable{
             return false;
         }
         return true;
+       
