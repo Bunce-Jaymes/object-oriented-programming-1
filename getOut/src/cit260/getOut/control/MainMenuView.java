202774fc49da5bf7dce32b9b7215452a20e4992d
Jaymes <35280742+PoisonedAlien@users.noreply.github.com>
2018-02-27 15:37:16 -0700

    Merge origin/master
    Conflicts:
    	getOut/nbproject/private/private.xml
    	getOut/src/cit260/getOut/control/doorControl.java
    	getOut/test/cit260/getOut/control/doorControlTest.java

--- getOut/src/cit260/getOut/control/MainMenuView.java
+++ getOut/src/cit260/getOut/control/MainMenuView.java
@@ -5,6 +5,9 @@
  */
 package cit260.getOut.control;
 
+import cit260.getOut.model.Player;
+import java.util.Scanner;
+
 /**
  *
  * @author jayme
@@ -12,6 +15,39 @@ package cit260.getOut.control;
 public class MainMenuView {
     
     public void displayMainMenuView(){
-        System.out.println("displayMainMenuView() called");
+      boolean endView = false;
+        do {
+            String[] inputs = getInputs();
+            String value = inputs[0].toUpperCase();
+            
+            if (value.equals('Q') || inputs.length < 1 ){
+                return;
+            }
+            endView = doAction(inputs);
+        } while (endView != true);
     }
+       private String[] getInputs() {
+        String[] inputs = new String[1];
+        //Display the instructions
+        //valid = false
+        //WHILE valid == false (while input value is not valid)
+        //Display the prompt message
+        //Get the value entered from the keyboard
+        //Trim off leading and trailing blanks from the value
+        //IF length of the value < 1 then
+        //Display "You must enter a value.”
+        //Continue
+        //ENDIF
+        //Assign value to the next position in the inputs array
+        //valid = true
+        //ENDWHILE
+        return inputs;
+    }
+
+    private boolean doAction(String[] inputs) {
+        System.out.println("Main menu doaction works");
+        System.out.println("\tinputs = " + inputs[0]);
+        return true;
+    }
+    
 }
