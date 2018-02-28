819c3317d4672f5a043dc3b1b88301f3449a6eba
Jaymes <35280742+PoisonedAlien@users.noreply.github.com>
2018-02-28 13:05:47 -0700

    Merge origin/master
    Conflicts:
    	getOut/nbproject/private/private.xml
    	getOut/src/cit260/getOut/control/doorControl.java
    	getOut/test/cit260/getOut/control/doorControlTest.java

--- getOut/src/cit260/getOut/control/helpMenuView.java
+++ getOut/src/cit260/getOut/control/helpMenuView.java
@@ -66,4 +66,19 @@ class helpMenuView {
        }
         return false;
     }
-}
+
+    private void goalOfTheGame() {
+        System.out.println("\nThis adventure takes place in an abandoned warehouse in the middle of Central America.\nYou wake up in a dark room, dazed, confused, and unaware how you got there.\nThe objective is to search the area and find a way to GET OUT.  You will need to locate objects in order to escape.\nEach victim has strengths and weaknesses that will be needed to overcome specific challenges.\n" +
+        "Along the way there will be a variety of obstacles including locked doors, puzzles.\n");
+    }
+
+    private void howToMove() {
+       System.out.println("\nEnter in coordinates based on where you want to move the character.\nEnter coordinates in this manner:  (A, 1).\nYou can only go to locations that have been opened on the map\n");
+    }
+
+    private void howDoorsOpen() {
+        System.out.println("\nThere are seven doors that you must unlock using a series of equations,\nitems or keys that are found throughout the building in order to escape the building.\nEach door you visit will prompt you to ask would you like to open the door.\nIt will either say it is unlocked or tell you the door is locked. \nIf it is locked it will display what you need to do to unlock.\nExample please find a key, or enter the pin, etc.\n");
+    }
+    private void howTools() {
+        System.out.println("\nOpen the game menu press T to view tools.\nYou can see all the tools that you have found so far.");
+}   }
