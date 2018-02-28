5a51aaa94b703986d647257c4c54b73cfb845aac
Jaymes <35280742+PoisonedAlien@users.noreply.github.com>
2018-02-28 13:41:40 -0700

    Merge origin/master
    Conflicts:
    	getOut/nbproject/private/private.xml
    	getOut/src/cit260/getOut/control/doorControl.java
    	getOut/test/cit260/getOut/control/doorControlTest.java

--- getOut/src/cit260/getOut/control/GameMenuView.java
+++ getOut/src/cit260/getOut/control/GameMenuView.java
@@ -5,6 +5,9 @@
  */
 package cit260.getOut.control;
 
+import cit260.getOut.view.ToolListView;
+import java.util.Scanner;
+
 /**
  *
  * @author jayme
@@ -24,18 +27,95 @@ class GameMenuView {
     }
 
     private String[] getInputs() {
-        System.out.println("This getInputs works");
+       String[] inputs = new String[1];
+        System.out.println("V - View Map\n" +
+        "M - Move player\n" +
+        "I - View inventory\n" +
+        "T - View list of tools\n" +
+        "E - Explore location\n" +
+        "S - Save game\n" +
+        "H - Help\n" +
+        "X - Exit game menu\n" +
+        "Q - Quit");
         
-        String[] inputs = new String[1];
-        inputs[0] = "gameMenuSelected";
-        
+        boolean valid = false;
+        while (valid == false){
+           System.out.println("Enter a key: ");
+           Scanner input;
+           input = new Scanner(System.in);
+           String scannedInput = input.nextLine();
+           String inputWithTrim = scannedInput.trim();
+           
+           if (inputWithTrim.length() <1){
+               System.out.println("You must enter a correct, non-blank value");
+               continue;
+           }
+           else{
+                inputs[0] = inputWithTrim;
+                valid = true;
+            }
+        }
         return inputs;
-    }
+
+        } 
 
     private boolean doAction(String[] inputs) {
-        System.out.println("This doaction works");
-        System.out.println("\tinputs = "+ inputs[0]);
-        
-        return true;
+      String menuItem = inputs[0];
+       menuItem = inputs[0].toUpperCase();
+       switch (menuItem) {
+           case "V": viewMap();
+           break;
+           case "M": movePlayer();
+           break;
+           case "I": viewInventory();
+           break;
+           case "T": viewToolList();
+           break;
+           case "E": exploreLocation();
+           break;
+           case "S": saveGame();
+           break;
+           case "H": helpMenu();
+           break;
+           case "X": return true;
+           case "Q": quit();
+           break;
+           
+           default : System.out.println("Invalid menu item.");
+       }
+        return false;
+    }
+
+    private void viewMap() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void movePlayer() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void viewInventory() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void viewToolList() {
+        ToolListView toolListView = new ToolListView();
+        toolListView.displayoolListView();
+    }
+
+    private void exploreLocation() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void saveGame() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void helpMenu() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void quit() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
